abf1ccfb81fdb71b217827af8aa95e1c1e81d0ba
Henrique de Paula <henrique.depaula@hotmail.com>
2024-03-24 14:09:45 -0300

    delete customer LGPD

--- application/src/main/kotlin/com/soat220/lanchonete/erp/driver/rest/CustomerController.kt
+++ application/src/main/kotlin/com/soat220/lanchonete/erp/driver/rest/CustomerController.kt
@@ -3,19 +3,19 @@ package com.soat220.lanchonete.erp.driver.rest
 import com.soat220.lanchonete.common.model.Customer
 import com.soat220.lanchonete.common.result.orThrow
 import com.soat220.lanchonete.erp.driver.rest.dto.request.CreateCustomerRequest
-import com.soat220.lanchonete.erp.usecase.CreateCustomer
-import com.soat220.lanchonete.erp.usecase.FindAllCustomers
-import com.soat220.lanchonete.erp.usecase.FindCustomerByCpf
-import com.soat220.lanchonete.erp.usecase.FindCustomerById
+import com.soat220.lanchonete.erp.driver.rest.dto.request.DeleteCustomerRequest
+import com.soat220.lanchonete.erp.usecase.*
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -25,7 +25,8 @@ class CustomerController(
     private val findCustomers: FindAllCustomers,
     private val findCustomerById: FindCustomerById,
     private val findCustomerByCpf: FindCustomerByCpf,
-    private val createCustomer: CreateCustomer
+    private val createCustomer: CreateCustomer,
+    private val deleteCustomer: DeleteCustomer
 ) {
     @GetMapping
     fun findCustomers() = findCustomers.execute().orThrow()
@@ -37,9 +38,9 @@ class CustomerController(
     fun createCustomer(@RequestBody createCustomerRequest: CreateCustomerRequest) =
         createCustomer.execute(createCustomerRequest.toDomain()).orThrow()
 
-    fun deleteCustomer(@RequestBody deleteCustomerRequest: CreateCustomerRequest) {
-
-    }
+    @DeleteMapping
+    fun deleteCustomer(@RequestBody deleteCustomerRequest: DeleteCustomerRequest) =
+        deleteCustomer.execute(deleteCustomerRequest.toDomain())
 
     @GetMapping("/find")
     fun findCustomerByCpf(@RequestParam cpf: String): ResponseEntity<Customer> {
